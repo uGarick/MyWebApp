9344ecb51a1f30e7838ddba1042b63976a07bd09
garickub <igor.gaz2004@gmail.com>
2021-11-29 18:17:50 +0300

    Flask DB, login, password, registration

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import requests
 import psycopg2
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -17,14 +17,33 @@ def index():
     return render_template('login.html')
 
 
-@app.route('/login/', methods=['POST'])
+@app.route('/login/', methods=['POST', 'GET'])
 def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    cursor.execute('SELECT * FROM service.users WHERE login=%s AND password=%s', (str(username), str(password)))
-    records = list(cursor.fetchall())
+    if request.method == 'POST':
+        if request.form.get("login"):
+            username = request.form.get('username')
+            password = request.form.get('password')
+            cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
+            records = list(cursor.fetchall())
+            return render_template('account.html', full_name=records[0][1])
+        elif request.form.get("registration"):
+            return redirect("/registration/")
 
-    return render_template('account.html', full_name=records[0][1])
+
+@app.route('/registration/', methods=['POST', 'GET'])
+def registration():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        login = request.form.get('login')
+        password = request.form.get('password')
+
+        cursor.execute('INSERT INTO service.users (full_name, login, password) VALUES (%s, %s, %s);',
+                       (str(name), str(login), str(password)))
+        conn.commit()
+
+        return redirect('/login/')
+
+    return render_template('registration.html')
 
 
 if __name__ == '__main__':
